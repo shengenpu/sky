84e091fd396efe1626a3e8fea4dbc04f93e07b8e
shen <2860924070@qq.com>
2026-02-15 04:23:05 +0800

    commit

--- sky-common/src/main/java/com/sky/constant/JwtClaimsConstant.java
+++ sky-common/src/main/java/com/sky/constant/JwtClaimsConstant.java
@@ -2,7 +2,7 @@ package com.sky.constant;
 
 public class JwtClaimsConstant {
 
-    public static final String EMP_ID = "empId";
+    public static final String EMP_ID = "empI000d";
     public static final String USER_ID = "userId";
     public static final String PHONE = "phone";
     public static final String USERNAME = "username";
